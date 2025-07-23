14f1ec4de5487eef097ed6eaf6d002093e3b604e
Arisings <96034376+Arisings@users.noreply.github.com>
2025-07-23 14:37:17 -0500

    Update ActionItem.js

--- Classes/ActionItem.js
+++ Classes/ActionItem.js
@@ -4,30 +4,42 @@ const ByteArrayInputStream = Java.type("java.io.ByteArrayInputStream");
 const CompressedStreamTools = Java.type("net.minecraft.nbt.CompressedStreamTools");
 
 export default class ActionItem {
+    /* 
+    0 -> Not an action item or invalid data
+    1 -> v1, has exposed action data
+    2 -> v2, has encrypted action data */
+    version = -1;
+
     constructor(itemStack) {
         if (itemStack == null) return this.item = null;
         this.item = new Item(itemStack);
+        this.version = this.validateVersion()
     }
 
-    isValid() {
-        return this.getActionData() != null;
+    validateVersion() {
+        if (this.getActionData()) {
+            return 1;
+        } else if (this.item.getNBT().getCompoundTag('tag')?.getCompoundTag('ExtraAttributes')?.getCompoundTag('interact_data')?.getString("data") && this.item.getNBT().getCompoundTag('tag')?.getCompoundTag('ExtraAttributes')?.getCompoundTag('interact_data')?.getInteger("version") == 2) { // v2 has interact_data
+            return 2;
+        } else {
+            return 0;
+        }
     }
 
     getInteractData() {
-        if (!this.hasInteractData()) return null;
-        return this.item.getNBT().getCompoundTag("tag").getCompoundTag("ExtraAttributes").getString("interact_data");
-    }
-
-    hasInteractData() {
-        if (!this.item) return false;
-        if (!this.item.getNBT().getCompoundTag("tag")) return false;
-        if (!this.item.getNBT().getCompoundTag("tag").getCompoundTag("ExtraAttributes")) return false;
-        if (!this.item.getNBT().getCompoundTag("tag").getCompoundTag("ExtraAttributes").getString("interact_data")) return false;
-        return true;
+        if (this.version === 1) {
+            return this.item.getNBT().getCompoundTag("tag").getCompoundTag("ExtraAttributes").getString("interact_data");
+        } else if (this.version === 2) {
+            return this.item.getNBT().getCompoundTag("tag").getCompoundTag("ExtraAttributes").getCompoundTag("interact_data").getString("data");
+        } else {
+            return null;
+        }
     }
 
     // Returns JSON object with action data
+    // ! Only meant to be used for v1 items !
     getActionData() {
+        if (this.version !== -1 && this.version !== 1) return null;
         try {
             const payload = this.getJWT()?.payload;
             if (!payload) return null
@@ -42,8 +54,11 @@ export default class ActionItem {
     }
 
     // Returns JSON object with keys 'header', 'payload', 'signature'
+    // ! Only meant to be used for v1 items !
     getJWT() {
-        if (!this.hasInteractData()) return null;
+        if (this.version !== -1 && this.version !== 1) return null;
+        if (!this.item) return false;
+        if (!this.item.getNBT()?.getCompoundTag("tag")?.getCompoundTag("ExtraAttributes")?.getString("interact_data")) return false;
         try {
             const actionToken = this.item.getNBT().getCompoundTag('tag').getCompoundTag('ExtraAttributes').getString('interact_data')
             const parts = actionToken.split('.');
